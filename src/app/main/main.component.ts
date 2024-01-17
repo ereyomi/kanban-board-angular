aca6fe615688356163de1dd4a3ccf85ef28fd83d
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2024-01-17 23:41:23 +0000

    feat: add column

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskListComponent } from '../shared/ui/task-list/task-list.component';
+import { TaskColumnModalComponent } from '../shared/ui/task-column-modal/task-column-modal.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [TaskListComponent],
+  imports: [TaskListComponent, TaskColumnModalComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
