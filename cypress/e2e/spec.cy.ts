6ee8d6b7538d9aa7ca616192a3ffcf55b69d6dc4
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2024-01-25 20:14:28 +0000

    refactor: e2e

--- cypress/e2e/spec.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/');
-    cy.contains('Ereyomi Board');
-    cy.get('#add_task_status').click();
-  });
-});
